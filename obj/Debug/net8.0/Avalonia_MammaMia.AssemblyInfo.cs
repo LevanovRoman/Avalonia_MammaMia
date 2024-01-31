0dc9ea4784f083d5c068d2006bf32319a2492274
levanovroman <levanovroman2016@yandex.ru>
2024-01-31 17:00:09 +0300

    second menu

--- obj/Debug/net8.0/Avalonia_MammaMia.AssemblyInfo.cs
+++ obj/Debug/net8.0/Avalonia_MammaMia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avalonia_MammaMia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0275aa86f36c16d1f13129919f10cbb0d145f029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+368a65cbf13d587c2f8a5f02a9d10164d09c05c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avalonia_MammaMia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avalonia_MammaMia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
